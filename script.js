9eb0e8726913858edc3c7ed838189692564dbbd4
mahdad <elmahdad@gmail.com>
2025-08-03 23:10:06 +0100

    retour normal

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+const randomNumberEl = document.querySelector("#random-number");
+
+      randomNumberEl.addEventListener("click", () => {
+        document.querySelector("#result").textContent = Math.floor(getRandomNumber(1, 100));
+      });
+
+
+      function getRandomNumber(min, max){
+
+        return Math.random() * (max - min) + min;
+      }
